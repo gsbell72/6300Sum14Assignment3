4d6274ec9894f34e0a5b8c4844042b3d170897f4
gsbell72 <gbell8@gatech.edi>
2014-06-14 11:27:01 -0400

    updates to handle the km to miles

--- UnitConvertor/src/edu/gatech/unitconvertor/DistanceActivity.java
+++ UnitConvertor/src/edu/gatech/unitconvertor/DistanceActivity.java
@@ -2,4 +2,14 @@ package edu.gatech.unitconvertor;
 
 public class DistanceActivity {
 
+	public String milesToKm(double miles) {
+		double km = miles * 1.609;
+		return String.valueOf(km);
+	}
+
+	public String kmToMiles(double km) {
+		double miles = km / 1.609;
+		return String.valueOf(miles);
+	}
+	
 }
--- UnitConvertor/src/edu/gatech/unitconvertor/MainActivity.java
+++ UnitConvertor/src/edu/gatech/unitconvertor/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +12,33 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+	
+	public String milesToKm(double miles) {
+		double km = miles * 1.609;
+		return String.valueOf(km);
+	}
+
+	public String kmToMiles(double km) {
+		double miles = km / 1.609;
+		return String.valueOf(miles);
+	}
+	
+	public void handleClick(View view) {
+		boolean checked = ((RadioButton) view).isChecked();
+		
+		EditText txt = (EditText) findViewById(R.id.Text1);
+		double distance = Double.parseDouble(txt.getText().toString());
+		
+		switch(view.getID()) {
+		case R.id.radio0:
+			if(checked)
+				txt.setText(kmToMiles(distance));
+			break;
+		case R.id.radio1:
+			if(checked)
+				txt.setText(milesToKm(distance));
+			break;
+		}
 	}
 }
